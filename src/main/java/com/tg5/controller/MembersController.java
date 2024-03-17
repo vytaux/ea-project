b2afe2464fdce89265ea5aba4dae113ab32c739f
Jean Claude GOMBANIRO <gombaniro@hotmail.com>
2024-03-17 13:37:52 -0500

    add data to payload and clean up

--- src/main/java/com/tg5/controller/MembersController.java
+++ src/main/java/com/tg5/controller/MembersController.java
@@ -1,22 +1,14 @@
 package com.tg5.controller;
 
-import com.tg5.domain.Event;
 import com.tg5.domain.Member;
-import com.tg5.domain.Role;
-import com.tg5.repository.MemberRepository;
-import com.tg5.service.contract.EventPayload;
 import com.tg5.service.contract.MemberPayload;
 import edu.miu.common.controller.BaseReadWriteController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/members")
-public class MembersController  extends BaseReadWriteController<MemberPayload, Member, Long> {
+public class MembersController extends BaseReadWriteController<MemberPayload, Member, Long> {
 
 //    private final MemberRepository memberRepository;
 //
