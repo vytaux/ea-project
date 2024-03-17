171208fb3343c0b7e52ca9ef8783cdaea3e9cc79
Vytautas A <vytautasasm@gmail.com>
2024-03-17 09:41:16 -0500

    disable csrf

--- src/main/java/com/tg5/service/contract/EventPayload.java
+++ src/main/java/com/tg5/service/contract/EventPayload.java
@@ -1,7 +1,10 @@
 package com.tg5.service.contract;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class EventPayload implements Serializable {
 
     private static final long serialVersionUID = 1L;
