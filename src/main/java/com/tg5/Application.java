5e34f3c2833ffa8a1c0953ad4d4d80b126c6c2d3
adonaygebrerufael <140199006+adonaygebrerufael@users.noreply.github.com>
2024-03-18 11:29:49 -0500

    i have added all layers.

--- src/main/java/com/tg5/Application.java
+++ src/main/java/com/tg5/Application.java
@@ -3,7 +3,7 @@ package com.tg5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "edu.miu.common, com.tg5")
+@SpringBootApplication(scanBasePackages = {"edu.miu.common", "com.tg5"})
 public class Application {
 
 	public static void main(String[] args) {
