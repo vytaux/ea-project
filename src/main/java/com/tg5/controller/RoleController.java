58700239e7b3ef66c136a9cfe5db77439ec06c84
Jean Claude GOMBANIRO <gombaniro@hotmail.com>
2024-03-19 08:51:11 -0500

    add data role

---
+++ src/main/java/com/tg5/controller/RoleController.java
@@ -0,0 +1,11 @@
+//package com.tg5.controller;
+//
+//import com.tg5.domain.Role;
+//import edu.miu.common.controller.BaseReadWriteController;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/roles")
+//public class RoleController extends BaseReadWriteController<RolePayload, Role, Long> {
+//}
