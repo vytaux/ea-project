216e103f4a18a239ea6fe6d34376727345669d21
Dip Ranjon Das <dip06ece@gmail.com>
2024-03-19 12:15:45 -0500

    Updated and bug fixed: CRUD test cases for Account

--- src/test/java/com/tg5/integration/controller/AccountsControllerTest.java
+++ src/test/java/com/tg5/integration/controller/AccountsControllerTest.java
@@ -2,14 +2,34 @@ package com.tg5.integration.controller;
 
 import com.tg5.integration.BaseTest;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.Test;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.*;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AccountsControllerTest extends BaseTest {
+    private static Integer createdAccountId;
 
     @Test
-    public void test() {
+    @Order(1)
+    public void testCreateAccount() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \"accountName\", \"description\": \"accountDescription\"}")
+                .when()
+                .post("/accounts")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+        createdAccountId = response.path("id");
+    }
+
+    @Test
+    @Order(2)
+    public void testReadAccounts() {
         given()
                 .log().all()
                 .when()
@@ -17,7 +37,36 @@ public class AccountsControllerTest extends BaseTest {
                 .then()
                 .log().body()
                 .contentType(ContentType.JSON)
-                .statusCode(200)
-        ;
+                .statusCode(200);
+    }
+    @Test
+    @Order(3)
+    public void testUpdateAccount() {
+        // Ensure that testCreateAccount test created an account
+        if (createdAccountId != null) {
+            given()
+                    .contentType(ContentType.JSON)
+                    .body("{\"id\":\""+createdAccountId+"\",\"name\": \"updatedAccount\", \"description\": \"updatedAccountDescription\"}")
+                    .when()
+                    .put("/accounts/"+createdAccountId) // Modify the ID according to your scenario
+                    .then()
+                    .statusCode(200);
+        } else {
+            throw new AssertionError("Update Test failed because condition is false!" + createdAccountId);
+        }
+    }
+    @Test
+    @Order(4)
+    public void testDeleteAccount() {
+        // Ensure that testCreateAccount test created an account
+        if (createdAccountId != null) {
+            given()
+                    .when()
+                    .delete("/accounts/" + createdAccountId)
+                    .then()
+                    .statusCode(200);
+        } else {
+            throw new AssertionError("DeleteTest failed because condition is false!" + createdAccountId);
+        }
     }
 }
