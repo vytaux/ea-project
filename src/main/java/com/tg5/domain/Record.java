ad320131b6208f622a0975f60799add8fa0d0aa1
ediyedu <ediyeedu@gmail.com>
2024-03-19 19:08:44 -0500

    committing changes made in record entity

--- src/main/java/com/tg5/domain/Record.java
+++ src/main/java/com/tg5/domain/Record.java
@@ -4,7 +4,12 @@ import jakarta.persistence.*;
 import lombok.Data;
 import com.tg5.domain.Record;
 import java.io.Serializable;
+import java.time.Instant;
+import java.time.LocalTime;
+
 import com.tg5.domain.Session;
+import org.hibernate.annotations.CreationTimestamp;
+
 @Entity
 @Table(name="records")
 @Data
@@ -12,6 +17,9 @@ public class Record implements Serializable {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+
+    @CreationTimestamp
+    private Instant scanTime;
 
     @ManyToOne
     private Member member;
