89fac2e97198df255c35230dc3ed69832e4cf6bb
Vytautas A <vytautasasm@gmail.com>
2024-03-21 02:03:06 -0500

    sessions_members link

--- src/main/java/com/tg5/service/contract/LocationPayload.java
+++ src/main/java/com/tg5/service/contract/LocationPayload.java
@@ -3,12 +3,18 @@ package com.tg5.service.contract;
 import com.tg5.domain.LocationType;
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
 public class LocationPayload implements Serializable {
+
+    @Serial
     private static final long serialVersionUID = 1L;
+
     private Long id;
+
     private String name;
+
     private LocationType locationType ;
 }
