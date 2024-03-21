702aec394bd800ec4e50b265fcb1f88cc2d39edd
Vytautas A <vytautasasm@gmail.com>
2024-03-20 22:00:36 -0500

    oauth

--- src/main/java/com/tg5/Application.java
+++ src/main/java/com/tg5/Application.java
@@ -1,7 +1,5 @@
 package com.tg5;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(Application.class, args);
 	}
 }
