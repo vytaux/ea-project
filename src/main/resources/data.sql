cd0304e63bc5c87a885ba75fd33f1d131d46cb8f
Vytautas A <vytautasasm@gmail.com>
2024-03-21 13:21:12 -0500

    preload data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,28 +1,24 @@
---  INSERT INTO members (id, firstname) VALUES (nextval('members_seq'), 'somename');
---  INSERT INTO members (id, firstname) VALUES (nextval('members_seq'), 'adonay');
---  INSERT INTO members (id, firstname) VALUES (nextval('members_seq'), 'Jean');
---  INSERT INTO members (id, firstname) VALUES (nextval('members_seq'), 'Claude');
---
---
---  INSERT INTO locations (id, name, location_type) VALUES (nextval('locations_seq'), 'Argiro', 'DINING_HALL');
---  INSERT INTO locations (id, name, location_type) VALUES (nextval('locations_seq'), 'Verill HALL', 'CLASSROOM');
---
---  INSERT INTO account_types (id, name, description, money_type, balance)
---  VALUES (nextval('account_types_seq'), 'CLASS', 'Lectures attendance', 'Points', 1000.0);
---
---  INSERT INTO account_types (id, name, description, money_type, balance)
---  VALUES (nextval('account_types_seq'), 'RESTAURANT', 'Food and Drinks', 'VirtualDollar', 4000.9);
---
--- INSERT INTO events (id)    VALUES (nextval('events_seq'));
---
--- -- INSERT INTO events_members(member_id,event_id)  VALUES (1,1);
--- -- INSERT INTO events_members(member_id,event_id)  VALUES (2,1);
---
--- INSERT INTO sessions (id, event_id) VALUES (nextval('sessions_seq'),1);
---  INSERT INTO sessions (id, event_id) VALUES (nextval('sessions_seq'),1);
+INSERT INTO members (id, firstname) VALUES (1, 'somename');
 
--- INSERT INTO records (session_id,member_id)  VALUES (1,1);
---  INSERT INTO records (session_id,member_id)  VALUES (1,2);
---  INSERT INTO records (session_id,member_id)  VALUES (2,2);
-SELECT * FROM members;
+INSERT INTO locations (id, name, location_type) VALUES (1, 'Argiro', 'DINING_HALL');
 
+INSERT INTO account_types (id, name, description, money_type, balance)
+    VALUES (1, 'CLASS', 'Lectures attendance', 'Points', 1000.0);
+
+-- User 1 has 2 accounts (CLASS and RESTAURANT)
+INSERT INTO accounts (id, member_id, account_type_id, balance) VALUES (1, 1, 1, 1000.0);
+
+-- Event 1 is a CLASS event, User 1 has registered for it, User 2 has registered for one session in it
+INSERT INTO events (id, location_id, account_type_id, start_date_time, end_date_time)
+    VALUES (1, 1, 1, '2024-01-01 10:00:00', '2024-02-01 12:00:00');
+
+-- User 1 has registered for event 1
+INSERT INTO events_members(member_id,event_id)  VALUES (1,1);
+
+-- Event 1 has 2 sessions
+INSERT INTO sessions (id, event_id) VALUES (1,1);
+INSERT INTO sessions (id, event_id) VALUES (2,1);
+
+INSERT INTO records (session_id,member_id)  VALUES (1,1);
+
+INSERT INTO scanners (id, location_id) VALUES (1, 1);
