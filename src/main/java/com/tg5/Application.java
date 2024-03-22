880436455b4e4f5e7aafadba5cf3d18b15e47094
Dip Ranjon Das <dip06ece@gmail.com>
2024-03-21 20:54:52 -0500

    Changing name of WAR file package

--- src/main/java/com/tg5/Application.java
+++ src/main/java/com/tg5/Application.java
@@ -1,15 +1,19 @@
 package com.tg5;
 
-import com.tg5.service.MessageConsumer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"edu.miu.common", "com.tg5"})
 @EnableScheduling
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 }
